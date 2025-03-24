76cc1b7bd48235331725fa15397fac05c2a593b3
trey1144 <trey1144@hotmail.com>
2025-03-23 19:40:35 -0600

    Phase03 videos

--- backend/WaterProject.API/Controllers/WaterController.cs
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -12,7 +12,7 @@ namespace WaterProject.API.Controllers
         public WaterController(WaterDbContext temp) => _waterContext = temp;
 
         [HttpGet("AllProjects")]
-        public IActionResult GetProjects(int pageSize = 10, int pageNum = 1)
+        public IActionResult GetProjects(int pageSize = 10, int pageNum = 1, [FromQuery] List<string>? projectTypes = null)
         {
             string? favProjType = Request.Cookies["favoriteProjectType"];
             Console.WriteLine("-------COOKIE-------\n" + favProjType);
@@ -25,12 +25,19 @@ namespace WaterProject.API.Controllers
                 Expires = DateTime.Now.AddHours(1)
             });
 
-            var something = _waterContext.Projects
+            var query = _waterContext.Projects.AsQueryable();
+
+            if (projectTypes != null && projectTypes.Any())
+            {
+                query = query.Where(p => projectTypes.Contains(p.ProjectType));
+            }
+
+            var totalNumProjects = query.Count();
+
+            var something = query
                 .Skip((pageNum-1) * pageSize)
                 .Take(pageSize)
                 .ToList();
-
-            var totalNumProjects = _waterContext.Projects.Count();
 
             var someObject = new 
             {
@@ -41,10 +48,17 @@ namespace WaterProject.API.Controllers
             return Ok(someObject);
         }
 
-        [HttpGet("FunctionalProjects")]
-        public IEnumerable<Project> GetFunctionalProjects()
+        [HttpGet("GetProjectTypes")]
+        public IActionResult GetProjectTypes()
         {
-            return _waterContext.Projects.Where(p => p.ProjectFunctionalityStatus == "Functional").ToList();
+            var projectTypes = _waterContext.Projects
+                .Select(p => p.ProjectType)
+                .Distinct()
+                .ToList();
+
+            return Ok(projectTypes);
         }
+
+
     }
 }
