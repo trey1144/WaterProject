5cd5892073fe5d16bf95b75a647b00e8cdb9c7d9
trey1144 <trey1144@hotmail.com>
2025-04-02 20:29:15 -0600

    final commits

--- backend/WaterProject.API/Program.cs
+++ backend/WaterProject.API/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins("http://localhost:3000", "https://wonderful-desert-01fe1471e.6.azurestaticapps.net")
                 .AllowCredentials()
                 .AllowAnyHeader()
                 .AllowAnyMethod();
